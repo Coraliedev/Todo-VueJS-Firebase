64e4c805186ff9e1306ea56f23456ee134e7b96b
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-12-15 20:17:17 +0100

    style: add todo

--- vue3-firebase-tutorial/src/App.vue
+++ vue3-firebase-tutorial/src/App.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Todo App</h1>
+  <div class="todos">
+    <div class="title has-text-centered">
+      Todo list
+    </div>
+    <div class="field has-addons">
+      <div class="control">
+        <input class="input" type="text" placeholder="Add a todo" />
+      </div>
+      <div class="control">
+        <a class="button is-info"> Search </a>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+@import "bulma/css/bulma.min.css";
+
+.todos {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
