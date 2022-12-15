d792429b5858565db37af627109ca7786060485b
Coralie Acquaert <coralie.acquaert@gmail.com>
2022-12-15 20:33:00 +0100

    style: todo list

--- vue3-firebase-tutorial/src/App.vue
+++ vue3-firebase-tutorial/src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="todos">
-    <div class="title has-text-centered">
-      Todo list
+    <div class="title has-text-centered">Todo list</div>
+    <div class="field is-grouped mb-5">
+      <p class="control is-expanded">
+        <input class="input" type="text" placeholder="Add todo" />
+      </p>
+      <p class="control">
+        <a class="button is-info"> Add </a>
+      </p>
     </div>
-    <div class="field has-addons">
-      <div class="control">
-        <input class="input" type="text" placeholder="Add a todo" />
-      </div>
-      <div class="control">
-        <a class="button is-info"> Search </a>
+    <div class="card mb-5">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">todo</div>
+            <div class="column is-5 has-text-right">
+              <button class="button is-light">&check;</button>
+              <button class="button is-danger ml-2">&cross;</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
